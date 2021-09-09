211ae097fdc893aced1e6fd03cfac2bfe3d8415f
javiermelendrez <81501034+javiermelendrez1@users.noreply.github.com>
2021-09-09 18:21:35 +0000

    created api fetch call

--- index.js
+++ index.js
@@ -2,9 +2,24 @@
 const discord = require('discord.js');
 const client = new discord.Client();
 //the client to work with for the server
+const fetch = require('node-fetch');
+
+
+const getQuote = () => {
+  //async fetch call to get the api 
+  //once it gets the api it will convert data into json format
+  //once converted it will format the data to "quote" - author
+  return fetch('https://zenquotes.io/api/random')
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    return data[0]['q']+' -' + data[0][''];
+  })
+}
+
 
 //ready event means the bot has connected to the server and is ready to do more things 
-
 client.on('ready', () => {
   console.log(`logged in as ${client.user.tag}`);
 });
