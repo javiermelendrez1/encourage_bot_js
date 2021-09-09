08d8181d11178561a725fae50ede54b96160ce2d
javiermelendrez <81501034+javiermelendrez1@users.noreply.github.com>
2021-09-09 17:47:16 +0000

    added listening functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const discord = require('discord.js');
+const client = new discord.client();
+//the client to work with for the server
+
+//ready event means the bot has connected to the server and is ready to do more things 
+
+client.on('ready', () => {
+  console.log(`logged in as ${client.uer.tag}`);
+});
+
+//going to listen for a message now
+client.on('message', (msg) => {
+  //check to see what the message is
+  if(msg.content === 'ping'){
+    msg.reply('pong');
+    //thew bot will reply with another message when listening for the that specific message
+  }
+});
